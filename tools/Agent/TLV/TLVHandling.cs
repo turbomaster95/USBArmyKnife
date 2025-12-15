43588a8ffa8f1f8f655551eaa3ee326b8ea10087
Deva Midhun <admin@coder.is-a.dev>
2025-12-15 16:06:28 +0530

    Update TLVHandling.cs

--- tools/Agent/TLV/TLVHandling.cs
+++ tools/Agent/TLV/TLVHandling.cs
@@ -5,7 +5,7 @@
         public static async Task<int> WriteTLVToStream(byte tag, byte[] data, int offset, int count, Stream output, CancellationToken token = default)
         {
             var bufferToSend = new byte[count + 4 + 1];
-            bufferToSend[0] = tag; // tag
+            bufferToSend[0] = tag; // tag LMAO for gh actions here lol
             var lenBytes = BitConverter.GetBytes(count);
             Buffer.BlockCopy(lenBytes, 0, bufferToSend, 1, lenBytes.Length);
             Buffer.BlockCopy(data, offset, bufferToSend, 1 + lenBytes.Length, count);
@@ -15,3 +15,4 @@
         }
     }
 }
+
